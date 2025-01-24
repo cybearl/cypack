685ddeebf727ddb73896f7b67c567ab2db54f47a
yoratoni <yoratoni.dev@gmail.com>
2025-01-25 00:22:33 +0100

    Working on the new logger system

---
+++ tests/sandbox.ts
@@ -0,0 +1,8 @@
+import logger from "../src/backend/logger"
+
+logger.trace("This is a message")
+logger.debug("This is a message")
+logger.info("This is a message")
+logger.warn("This is a message")
+logger.error("This is a message")
+logger.fatal("This is a message")
