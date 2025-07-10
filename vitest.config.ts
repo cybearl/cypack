b4ebe65387295bb4741d06b8ec3a23923ffecc1a
yoratoni <yoratoni.dev@gmail.com>
2025-07-10 11:57:23 +0200

    fix: Small improvements to the configs & conventions

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,7 @@ import tsconfigPaths from "vite-tsconfig-paths"
 import { defineConfig } from "vitest/config"
 
 /**
- * The main configuration for the Vitest tests of the workers.
+ * The main configuration for the Vitest tests.
  */
 const vitestConfig = defineConfig({
 	plugins: [tsconfigPaths()],
