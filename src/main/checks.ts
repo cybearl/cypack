922ca35442749747694164663df4319da9814fcc
yoratoni <yoratoni.dev@gmail.com>
2026-03-02 16:14:16 +0100

    refactor: fix order for is server and is client functions

--- src/main/checks.ts
+++ src/main/checks.ts
@@ -1,15 +1,15 @@
 /**
- * Returns true if the code is running in a client environment.
- */
-export function isClient() {
-    return !isServer()
-}
-
-/**
  * Returns true if the code is running in a server environment.
  */
 export function isServer() {
     return typeof window === "undefined"
+}
+
+/**
+ * Returns true if the code is running in a client environment.
+ */
+export function isClient() {
+    return !isServer()
 }
 
 /**
